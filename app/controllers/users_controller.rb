0fa99f8207b2cd4b738a09f125117490afc7df3c
yuta.takahashi <sb.yuta.takahashi@gmail.com>
2020-12-12 14:03:34 +0000

    favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  def likes
+    @user = User.find(params[:id])
+    @favorites = current_user.likes.page(params[:page])
+    counts(@user)
+  end
+  
 
    private
 
